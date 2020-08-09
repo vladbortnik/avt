ed50efabf6cc869eacc9411ff9551fa6dfc8ea68
vladbortnik <vladbortnik@icloud.com>
2020-08-08 20:55:03 -0400

    indentation edit

--- api.py
+++ api.py
@@ -2,33 +2,33 @@ from storage import Storage
 from pprint import pprint
 
 class API:
-	def __init__(self, storage, key):
-		self.storage = storage
-		self.key = key
+    def __init__(self, storage, key):
+        self.storage = storage
+        self.key = key
 
-	def decorator(function):
-		def wrapper(self, *args, **kwargs):
-			if self.key != kwargs['key']:
-				return 403
-			return function(self, *args, **kwargs)
-		return wrapper
+    def decorator(function):
+        def wrapper(self, *args, **kwargs):
+            if self.key != kwargs['key']:
+                return 403
+            return function(self, *args, **kwargs)
+        return wrapper
 
 
-	@decorator
-	def post(self, user, key):
-		return self.storage.create(user)
+    @decorator
+    def post(self, user, key):
+        return self.storage.create(user)
 
-	@decorator
-	def get(self, user_id, key):
-		return self.storage.read(user_id)
+    @decorator
+    def get(self, user_id, key):
+        return self.storage.read(user_id)
 
-	@decorator
-	def update(self, user_id, user, key):
-		return self.storage.update(user_id, user)
+    @decorator
+    def update(self, user_id, user, key):
+        return self.storage.update(user_id, user)
 
-	@decorator
-	def delete(self, user_id, key):
-		return self.storage.delete(user_id)
+    @decorator
+    def delete(self, user_id, key):
+        return self.storage.delete(user_id)
 
 
 # TEMPLATE: HOW TO PASS UNKNOWN ARGUMENTS TO THE DECORATOR
--- storage.py
+++ storage.py
@@ -3,70 +3,70 @@ from uuid import uuid4
 from pprint import pprint
 
 class Storage:
-	def __init__(self, filename):
-		self.filename = filename
+    def __init__(self, filename):
+        self.filename = filename
 
-	def create(self, user):
-		user['id'] = str(uuid4())
-		file_object = open(self.filename, 'a')
-		json_data = json.dumps(user)	
-		file_object.write(json_data + '\n')
-		file_object.close()
-		return user['id']
+    def create(self, user):
+        user['id'] = str(uuid4())
+        file_object = open(self.filename, 'a')
+        json_data = json.dumps(user)    
+        file_object.write(json_data + '\n')
+        file_object.close()
+        return user['id']
 
-	def read(self, user_id):
-		file_object = open(self.filename, 'r')
-		for line in file_object.readlines():
-			cur_user = json.loads(str(line))
-			if cur_user['id'] == user_id:
-				file_object.close()
-				return cur_user
-		file_object.close()
-		return False
+    def read(self, user_id):
+        file_object = open(self.filename, 'r')
+        for line in file_object.readlines():
+            cur_user = json.loads(str(line))
+            if cur_user['id'] == user_id:
+                file_object.close()
+                return cur_user
+        file_object.close()
+        return False
 
-	def update(self, user_id, user):
-		with open(self.filename, 'r') as file_object:
-			users = []
-			for line in file_object.readlines():
-				cur_user = json.loads(line)
-				users.append(cur_user)
+    def update(self, user_id, user):
+        with open(self.filename, 'r') as file_object:
+            users = []
+            for line in file_object.readlines():
+                cur_user = json.loads(line)
+                users.append(cur_user)
 
-		def is_user(user):
-			return user['id'] == user_id
+        def is_user(user):
+            return user['id'] == user_id
 
-# LIST COMPREHENTION		
+# LIST COMPREHENTION        
 # new_list = [new_element if condition else old_element for old_element in old_list]
 
-		users = [dict(old_user, **user) 
-				 if old_user['id'] == user_id
-				 else old_user
-				 for old_user in users]
+        users = [dict(old_user, **user) 
+                 if old_user['id'] == user_id
+                 else old_user
+                 for old_user in users]
 
-		if not list(filter(is_user, users)):
-			return None
+        if not list(filter(is_user, users)):
+            return None
 
-		with open(self.filename, 'w') as file_object:
-			for user in users:
-				json_data = json.dumps(user)
-				file_object.write(json_data + '\n')
+        with open(self.filename, 'w') as file_object:
+            for user in users:
+                json_data = json.dumps(user)
+                file_object.write(json_data + '\n')
 
-		return user_id
+        return user_id
 
-	def delete(self, user_id):
-		users = []
-		with open(self.filename, 'r') as file_object:
-			for line in file_object.readlines():
-				users.append(json.loads(line))
+    def delete(self, user_id):
+        users = []
+        with open(self.filename, 'r') as file_object:
+            for line in file_object.readlines():
+                users.append(json.loads(line))
 
-		def is_not_user(user):
-			return user['id'] != user_id
-		filtered_list = list(filter(is_not_user, users))
+        def is_not_user(user):
+            return user['id'] != user_id
+        filtered_list = list(filter(is_not_user, users))
 
-		with open(self.filename, 'w') as file_object:
-			for user in filtered_list:
-				file_object.write(json.dumps(user) + '\n')
+        with open(self.filename, 'w') as file_object:
+            for user in filtered_list:
+                file_object.write(json.dumps(user) + '\n')
 
-		return user_id
+        return user_id
 
 
 
